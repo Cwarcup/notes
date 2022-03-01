45ac13d4c03e45c85c380c45cdc175678afc6726
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-01 13:06:10 -0800

    src files added

---
+++ root/typescript/TS-Projects/maps/src/CustomMap.ts
@@ -0,0 +1,39 @@
+// Instructions to every other class on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+export class CustomMap {
+  //mark as private so nobody else can use the instance of googleMap
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
+      center: { lat: 0, lng: 0},
+      zoom: 1,
+    });
+  }
+  //adding a marker
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker)
+    })
+  }
+}
+
--- root/typescript/TS-Projects/maps/src/User.ts
+++ root/typescript/TS-Projects/maps/src/User.ts
@@ -1,11 +1,13 @@
 import faker from '@faker-js/faker';
+import { Mappable } from './CustomMap';
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'blue'
 
   constructor() {
     this.name = faker.name.firstName();
@@ -14,5 +16,8 @@ export class User {
       lng: parseFloat(faker.address.longitude())
     }
   }
-}
 
+  markerContent(): string {
+    return `User Name: ${this.name}`
+  }
+}
