cc62f20789475b5ce86dcc5fd29caddda17335b0
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-26 15:18:08 -0700

    tree notes

--- root/Algorithms-Masterclass/Algo-Data-Structures-bootcamp/exercises/stack/index.js
+++ root/Algorithms-Masterclass/Algo-Data-Structures-bootcamp/exercises/stack/index.js
@@ -4,27 +4,55 @@
 // 'peek'.  Adding an element to the stack should
 // store it until it is removed.
 // --- Examples
-//   const s = new Stack();
-//   s.push(1);
-//   s.push(2);
-//   s.pop(); // returns 2
-//   s.pop(); // returns 1
 
 class Stack {
   constructor() {
     this.data = [];
   }
 
-  push(value) {
-    this.data.push(value);
+  push(data) {
+    // add data to end
+    this.data.push(data);
   }
 
   pop() {
+    // remove data from end of array
     return this.data.pop();
   }
+
   peek() {
+    // see last data point
     return this.data[this.data.length - 1];
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
   }
 }
 
-module.exports = Stack;
+const s = new Stack();
+s.push(1);
+s.push(2);
+console.log(s.peek()); // returns 2
+console.log(s.pop());
+console.log(s.pop());
+console.log(s.isEmpty()); // returns 1
+
+// class Stack {
+//   constructor() {
+//     this.data = [];
+//   }
+
+//   push(value) {
+//     this.data.push(value);
+//   }
+
+//   pop() {
+//     return this.data.pop();
+//   }
+//   peek() {
+//     return this.data[this.data.length - 1];
+//   }
+// }
+
+// module.exports = Stack;
